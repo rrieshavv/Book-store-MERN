caa21995304abee4e130034713f830f16acaf661
Rishav Karna <asrishavaf@gmail.com>
2023-09-10 22:41:13 +0545

    routes for books created with logics

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDB_URL = "mongodb://127.0.0.1:27017/book-store";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+
+import { PORT, mongoDB_URL } from "./config.js";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening to port: ${PORT}`);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is the default route");
 });
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDB_URL)
+  .then(() => {
+    console.log(`App connected to database`);
+    app.listen(PORT, () => {
+      console.log(`Server is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
